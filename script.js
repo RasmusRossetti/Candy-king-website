58c4e72c094d2780fec911f4c15dbd15f60bde3e
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2021-11-16 00:10:29 +0100

    changed navbar to still

--- script.js
+++ script.js
@@ -14,12 +14,11 @@ let product = [
    { image:"/img/cotton-candy.png" , name:'Cotton Candy', price:35, info: 'One bite of these puffy cumulus clouds of sugar floss, raspberry-flavored blue cotton candy, will have your taste buds floating on cloud nine! Hosting a baby shower or other event with a blue color theme? '},
    { image:"/img/gummy-bear.png" , name:'Gummy Bears', price:40, info: 'High-brow bears with sophisticated fruity flavors, these delicious candy creatures are not your everyday gummy bear. You"ll want to save these sweets for your favorite friends and family.'},
    { image:"/img/lollipop.png" , name:'Lollipop', price:5, info: 'Well, we can assure you that your suddenly growling stomach has as much to do with this hypnotic pattern as it does with the fact that these little lollipops look just as delicious as they taste!'}
-  
 ];
 
 
 product.forEach(element => {document.getElementById('output').innerHTML +=( `<div class="card" style="width: 18rem;">
-<img src="${element.image}" class="card-img-top" alt="...">
+<img src="${element.image}" class="card-img-top" alt="Candy">
 <div class="card-body">
   <h5 id="product1">${element.name}<br>${element.price}$ </h5>
   <p class="card-text">${element.info}</p>
@@ -28,5 +27,23 @@ product.forEach(element => {document.getElementById('output').innerHTML +=( `<di
 </div>`)
 });
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
